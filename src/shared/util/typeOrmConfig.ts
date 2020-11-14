a6326f6aba62b9d4f88a1006c8b11bf6e6613135
User <kljopuu@gmail.com>
2020-11-15 00:34:09 +0900

    initial settings

---
+++ src/shared/util/typeOrmConfig.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const env = process.env.NODE_ENV;
+
+let host = 'localhost';
+
+if (env !== 'dev') host = '172.16.25.2';
+
+export const typeormConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  port: 5432,
+  username: 'testuser',
+  password: 'testpasswd',
+  database: 'testdb',
+  synchronize: true,
+  entities: [`${path.join(__dirname, '..', '..', '**')}/*.model.[tj]s`],
+  host: "localhost",
+};
